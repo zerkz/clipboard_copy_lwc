3e99d6f2f56fa77696ba6d93b7adb20771b4b1e8
Zack Whipkey <zack.whipkey@gmail.com>
2025-01-15 22:55:32 -0600

    Init

---
+++ force-app/main/default/lwc/clipboardCopy/clipboardCopy.js
@@ -0,0 +1,108 @@
+import { LightningElement, api } from 'lwc';
+/**
+ * A LWC Component that copies content to the clipboard. Component can be shown
+ */
+export default class ClipboardCopy extends LightningElement {
+    //default values
+    
+    //general
+    _type = 'icon'; // if not icon, defaults to lightning button
+    _iconName = 'utility:copy_to_clipboard';
+    _content = '';
+    _variant = 'base';
+    _cssClass = '';
+    _title = 'Copy To Clipboard'
+
+    //lightning-button-icon attributes    
+    _iconSize = 'medium';
+
+    //lightning button attributes
+    _label = 'Copy To Clipboard';
+    _iconPosition = 'left'
+    _stretch = false;
+
+    //event listener functions
+    copyToClipboard() {
+       let textArea = this.refs.clipboardTextArea;
+       textArea.select();
+       document.execCommand('copy');
+       if (window.getSelection) {
+        window.getSelection().removeAllRanges();
+       } else if (document.selection) {
+        document.selection.empty();
+       }
+    }
+
+    get isIconButton() {
+        return this._type === 'icon';
+    }
+
+
+    @api
+    set iconName(value) {
+        this._iconName = value;
+    }
+
+    get iconName() {
+        return this._iconName;
+    }
+
+    @api
+    set iconSize(value) {
+        this._iconSize = value;
+    }
+
+    get iconSize() {
+        return this._iconSize;
+    }
+
+    @api
+    set variant(value) {
+        this._variant = value;
+    }
+
+    get variant() {
+        return this._variant;
+    }
+
+    @api
+    set cssClass(value) {    
+        this._cssClass = value;
+    }
+
+    get cssClass() {
+        return this._cssClass;
+    }
+
+    @api 
+    set content(value) {
+        this._content = value;
+    }
+
+    get content() {
+        return this._content;
+    }
+
+    @api 
+    set label(value) {
+        this._label = value;
+    }
+
+    get label() {
+        return this._label;
+    }
+
+    @api
+    set title(value) {
+        this._title = value;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+
+
+
+
+}
